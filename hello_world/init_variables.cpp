8c11afd7b428f489f957827e7652cc7ef6c6a743
alvarffff <alvar.fernandezv@gmail.com>
2024-10-02 11:39:51 +0200

    Test how to init variables in C++

---
+++ hello_world/init_variables.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    int x{5};
+    int y(4);
+    int z = 10;
+    std::cout << "Hello world\n";
+
+    std::cout << "Number 1 is: " << x;
+    std::cout << "Number 2 is: " << y;
+    std::cout << "Number 3 is: " << z;
+}
